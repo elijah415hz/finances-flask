d6e3837b3a0fabceb0ee35eb6fedf96bef66a6ef
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2021-03-02 22:06:26 -0800

    Fixed key errors...

--- cron.py
+++ cron.py
@@ -38,11 +38,11 @@ if today.day == 3:
     expensesList = [
         {'vendor': 'Google Play', 'Amount': 1.99, 'broad_category_id': 5, 'narrow_category_id': 6, 'person_id': None, 'notes': ""},
         {'vendor': 'New York Times', 'Amount': 4, 'broad_category_id': 14, 'narrow_category_id': 47, 'person_id': None, 'notes': ""},
-        {'vendor': 'Planned parenthood', 'Amount': 10, 'broad_category_id': 17, 'person_id': None, 'notes': ""},
+        {'vendor': 'Planned parenthood', 'Amount': 10, 'broad_category_id': 17, 'narrow_category_id': None, 'person_id': None, 'notes': ""},
         {'vendor': 'Xfinity', 'Amount': 45.00, 'broad_category_id': 7, 'narrow_category_id': 1, 'person_id': None, 'notes': ""},
         {'vendor': 'Ives HOA', 'Amount': 301, 'broad_category_id': 7, 'narrow_category_id': 38, 'person_id': None, 'notes': ""},
         {'vendor': 'Caliber', 'Amount': 1068.02, 'broad_category_id': 7, 'narrow_category_id': 39, 'person_id': None, 'notes': ""},
-        {'vendor': 'Wells Student Loan', 'Amount': 151.54, 'broad_category_id': 15, 'person_id': None, 'notes': ""},
+        {'vendor': 'Wells Student Loan', 'Amount': 151.54, 'broad_category_id': 15, 'narrow_category_id': None, 'person_id': None, 'notes': ""},
         {'vendor': 'Apple iCloud', 'Amount': 2.99, 'broad_category_id': 5, 'narrow_category_id': 6, 'person_id': None, 'notes': ""},
         {'vendor': 'Makespace', 'Amount': 325.21, 'broad_category_id': 1, 'narrow_category_id': 40, 'person_id': None, 'notes': ""},
         {'vendor': 'Spotify', 'Amount': 10.99, 'broad_category_id': 14, 'narrow_category_id': 46, 'person_id': None, 'notes': ""},
