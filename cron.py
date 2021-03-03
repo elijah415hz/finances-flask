fae3894e5dda2a2464c92707f874d088d2f0cec7
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2021-03-02 21:32:37 -0800

    Added notes fields

--- cron.py
+++ cron.py
@@ -8,18 +8,18 @@ print(f"Today is the {today.day}th day of the month")
 if today.day == 3:
     print("Loading recurring expenses...")
     expensesList = [
-        {'vendor': 'Google Play', 'Amount': 1.99, 'broad_category_id': 5, 'narrow_category_id': 6, 'person_id': None},
-        {'vendor': 'New York Times', 'Amount': 4, 'broad_category_id': 14, 'narrow_category_id': 47, 'person_id': None},
-        {'vendor': 'Planned parenthood', 'Amount': 10, 'broad_category_id': 17, 'person_id': None},
-        {'vendor': 'Xfinity', 'Amount': 45.00, 'broad_category_id': 7, 'narrow_category_id': 1, 'person_id': None},
-        {'vendor': 'Ives HOA', 'Amount': 301, 'broad_category_id': 7, 'narrow_category_id': 38, 'person_id': None},
-        {'vendor': 'Caliber', 'Amount': 1068.02, 'broad_category_id': 7, 'narrow_category_id': 39, 'person_id': None},
-        {'vendor': 'Wells Student Loan', 'Amount': 151.54, 'broad_category_id': 15, 'person_id': None},
-        {'vendor': 'Apple iCloud', 'Amount': 2.99, 'broad_category_id': 5, 'narrow_category_id': 6, 'person_id': None},
-        {'vendor': 'Makespace', 'Amount': 325.21, 'broad_category_id': 1, 'narrow_category_id': 40, 'person_id': None},
-        {'vendor': 'Spotify', 'Amount': 10.99, 'broad_category_id': 14, 'narrow_category_id': 46, 'person_id': None},
-        {'vendor': 'Netflix', 'Amount': 9.90, 'broad_category_id': 14, 'narrow_category_id': 46, 'person_id': None},
-        {'vendor': 'Xfinity Mobile', 'Amount': 36.20, 'broad_category_id': 5, 'narrow_category_id': 33, 'person_id': None} 
+        {'vendor': 'Google Play', 'Amount': 1.99, 'broad_category_id': 5, 'narrow_category_id': 6, 'person_id': None, 'notes': ""},
+        {'vendor': 'New York Times', 'Amount': 4, 'broad_category_id': 14, 'narrow_category_id': 47, 'person_id': None, 'notes': ""},
+        {'vendor': 'Planned parenthood', 'Amount': 10, 'broad_category_id': 17, 'person_id': None, 'notes': ""},
+        {'vendor': 'Xfinity', 'Amount': 45.00, 'broad_category_id': 7, 'narrow_category_id': 1, 'person_id': None, 'notes': ""},
+        {'vendor': 'Ives HOA', 'Amount': 301, 'broad_category_id': 7, 'narrow_category_id': 38, 'person_id': None, 'notes': ""},
+        {'vendor': 'Caliber', 'Amount': 1068.02, 'broad_category_id': 7, 'narrow_category_id': 39, 'person_id': None, 'notes': ""},
+        {'vendor': 'Wells Student Loan', 'Amount': 151.54, 'broad_category_id': 15, 'person_id': None, 'notes': ""},
+        {'vendor': 'Apple iCloud', 'Amount': 2.99, 'broad_category_id': 5, 'narrow_category_id': 6, 'person_id': None, 'notes': ""},
+        {'vendor': 'Makespace', 'Amount': 325.21, 'broad_category_id': 1, 'narrow_category_id': 40, 'person_id': None, 'notes': ""},
+        {'vendor': 'Spotify', 'Amount': 10.99, 'broad_category_id': 14, 'narrow_category_id': 46, 'person_id': None, 'notes': ""},
+        {'vendor': 'Netflix', 'Amount': 9.90, 'broad_category_id': 14, 'narrow_category_id': 46, 'person_id': None, 'notes': ""},
+        {'vendor': 'Xfinity Mobile', 'Amount': 36.20, 'broad_category_id': 5, 'narrow_category_id': 33, 'person_id': None, 'notes': ""} 
     ]
     for expense in expensesList:
         today_str = today.strftime("%m/%d/%Y")
