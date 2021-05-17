a74f09b7c51fa3f08d88540e75bfffd6ca840563
Elijah Blaisdell <69051669+elijah415hz@users.noreply.github.com>
2021-05-17 14:45:59 -0400

    chore: updated recurring expenses

--- cron.py
+++ cron.py
@@ -39,15 +39,14 @@ if today.day == 3:
         {'vendor': 'Google Play', 'Amount': 1.99, 'broad_category_id': 5, 'narrow_category_id': 6, 'person_id': None, 'notes': ""},
         {'vendor': 'New York Times', 'Amount': 14, 'broad_category_id': 14, 'narrow_category_id': 47, 'person_id': None, 'notes': ""},
         {'vendor': 'Planned parenthood', 'Amount': 10, 'broad_category_id': 17, 'narrow_category_id': None, 'person_id': None, 'notes': ""},
-        {'vendor': 'Xfinity', 'Amount': 45.00, 'broad_category_id': 7, 'narrow_category_id': 1, 'person_id': None, 'notes': ""},
+        {'vendor': 'Xfinity', 'Amount': 60.44, 'broad_category_id': 7, 'narrow_category_id': 1, 'person_id': None, 'notes': ""},
         {'vendor': 'Ives HOA', 'Amount': 301, 'broad_category_id': 7, 'narrow_category_id': 38, 'person_id': None, 'notes': ""},
         {'vendor': 'Caliber', 'Amount': 1068.02, 'broad_category_id': 7, 'narrow_category_id': 39, 'person_id': None, 'notes': ""},
         {'vendor': 'Wells Student Loan', 'Amount': 151.54, 'broad_category_id': 15, 'narrow_category_id': None, 'person_id': None, 'notes': ""},
         {'vendor': 'Apple iCloud', 'Amount': 2.99, 'broad_category_id': 5, 'narrow_category_id': 6, 'person_id': None, 'notes': ""},
-        {'vendor': 'Makespace', 'Amount': 325.21, 'broad_category_id': 1, 'narrow_category_id': 40, 'person_id': None, 'notes': ""},
         {'vendor': 'Spotify', 'Amount': 10.99, 'broad_category_id': 14, 'narrow_category_id': 46, 'person_id': None, 'notes': ""},
         {'vendor': 'Netflix', 'Amount': 9.90, 'broad_category_id': 14, 'narrow_category_id': 46, 'person_id': None, 'notes': ""},
-        {'vendor': 'Xfinity Mobile', 'Amount': 36.20, 'broad_category_id': 5, 'narrow_category_id': 33, 'person_id': None, 'notes': ""} 
+        {'vendor': 'Xfinity Mobile', 'Amount': 36.42, 'broad_category_id': 5, 'narrow_category_id': 33, 'person_id': None, 'notes': ""} 
     ]
     for expense in expensesList:
         today_str = today.strftime("%m/%d/%Y")
