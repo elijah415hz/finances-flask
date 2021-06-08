8ff53aec657349f3e947e0bfcc3f882b417f2b23
Elijah Blaisdell <69051669+elijah415hz@users.noreply.github.com>
2021-06-08 13:04:59 -0700

    chore: added sqlalchemy connection string

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 import os
 
-FLASK_DB_URI = os.environ.get("FLASK_DB_URI")
+
+
+FLASK_DB_URI = "mysql+pymysql://blarvis:twenty2?@finances:3306/finances_db" # os.environ.get("FLASK_DB_URI")
 
 # Create database connection
 engine = create_engine(FLASK_DB_URI) 
