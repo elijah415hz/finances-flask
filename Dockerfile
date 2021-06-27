4f1d73cde6520107039c9c6d587be84dd4253fd8
Elijah Blaisdell <69051669+elijah415hz@users.noreply.github.com>
2021-06-26 19:53:38 -0700

    feature: added docker-compose support

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,4 @@ COPY requirements.txt ./
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
-
 CMD ["gunicorn"  , "-b", "0.0.0.0:8000", "wsgi:app"]
--- db/create_mysql_finances.sql
+++ db/create_mysql_finances.sql
@@ -1,55 +1,60 @@
-CREATE DATABASE IF NOT EXISTS finances_db CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+CREATE DATABASE finances_db CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
 USE finances_db;
 
-CREATE TABLE IF NOT EXISTS expenses (
+CREATE TABLE expenses (
     entry_id INT NOT NULL AUTO_INCREMENT,
     date DATE,
     vendor_id INT,
     amount FLOAT,
-    broad_category_id SMALLINT,
-    narrow_category_id SMALLINT,
+    broad_category_id INT,
+    narrow_category_id INT,
     person_id INT,
     notes VARCHAR(100),
     PRIMARY KEY (entry_id)
 );
 
-CREATE TABLE IF NOT EXISTS vendor (
+CREATE TABLE vendor (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50),
     PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX vendor_name ON vendor(name);
 
-CREATE TABLE IF NOT EXISTS broad_category (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
+CREATE TABLE broad_category (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX b_name ON broad_category(name);
 
-CREATE TABLE IF NOT EXISTS narrow_category (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE narrow_category (
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50),
+    PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX n_name ON narrow_category(name);
 
-CREATE TABLE IF NOT EXISTS income (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE income (
+    id INT NOT NULL AUTO_INCREMENT,
     date DATE,
     amount FLOAT,
     source_id INT,
     earner_id INT,
-    FOREIGN KEY (source_id) REFERENCES source(id),
-    FOREIGN KEY (person_id) REFERENCES person_earner(id)
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS source (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
+CREATE TABLE source (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX s_name ON source(name);
 
-CREATE TABLE IF NOT EXISTS person_earner (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+CREATE TABLE person_earner (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX pe_name ON person_earner(name);
+
+
